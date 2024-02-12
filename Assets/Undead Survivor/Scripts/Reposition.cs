f8a89f727d6cfbc5d2e18684b2938d26ce1f4d85
정은채 <ddolchae513@gmail.com>
2024-02-12 21:15:07 +0900

    Create Enemy
    - Player를 따라가도록 구현
    - Player 와의 Mass 조절, Z rotation constraint
    - Reposition 기능 추가

--- Assets/Undead Survivor/Scripts/Reposition.cs
+++ Assets/Undead Survivor/Scripts/Reposition.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Reposition : MonoBehaviour
 {
+    Collider2D coll;
+
+    private void Awake()
+    {
+        coll = GetComponent<Collider2D>();
+    }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (!collision.CompareTag("Area"))
@@ -31,6 +38,10 @@ public class Reposition : MonoBehaviour
                 }
                 break;
             case "Enemy":
+                if (coll.enabled)
+                {
+                    transform.Translate(playerDir * 20 + new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), 0));
+                }
                 break;
         }
     }
